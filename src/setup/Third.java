0cf6340afa4d66a16ba6f8fe4a10ffd1584283b9
fyrzsh <shahmuradov@aol.com>
2021-10-03 12:49:45 -0400

    Updated third file again

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -3,14 +3,14 @@ package setup;
 public class Third {
 
     public void m () {
-
+        System.out.println("third one");
     }
 
     public void m (int n) {
-
+        System.out.println("third two");
     }
 
     public void m (String s){
-
+        System.out.println("third three");
     }
 }

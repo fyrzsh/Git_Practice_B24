990c7fe5cd390f754e12d4dddc545c8cb7dcaeac
fyrzsh <shahmuradov@aol.com>
2021-10-03 13:11:37 -0400

    Added two methods

---
+++ src/setup/Fourth.java
@@ -0,0 +1,13 @@
+package setup;
+
+public class Fourth {
+    // Adding notes
+    public void newMethods () {
+
+    }
+
+    // Adding more notes
+    public void newMethods (String s) {
+
+    }
+}
